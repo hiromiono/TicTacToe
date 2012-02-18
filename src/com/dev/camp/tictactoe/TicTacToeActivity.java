28c2254b1e4353017912decb3fdf59cef3a2b124
Ronald Petty <ronald.petty@minimumdistance.com>
2012-02-17 22:15:23 -0800

    Testing commit

--- src/com/dev/camp/tictactoe/TicTacToeActivity.java
+++ src/com/dev/camp/tictactoe/TicTacToeActivity.java
@@ -8,6 +8,6 @@ public class TicTacToeActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main); 
     }
 }
