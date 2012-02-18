3812214bcdf6529489a8bbd10b0f5b55217050dd
Safe Steps <safesteps@gmail.com>
2012-02-18 11:21:20 -0800

    a grid view

--- gen/com/dev/camp/tictactoe/R.java
+++ gen/com/dev/camp/tictactoe/R.java
@@ -13,10 +13,24 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int b1=0x7f050001;
+        public static final int b2=0x7f050002;
+        public static final int b3=0x7f050003;
+        public static final int b4=0x7f050004;
+        public static final int b5=0x7f050005;
+        public static final int b6=0x7f050006;
+        public static final int b7=0x7f050007;
+        public static final int b8=0x7f050008;
+        public static final int b9=0x7f050009;
+        public static final int gridLayout2=0x7f050000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
+        public static final int O=0x7f040004;
+        public static final int X=0x7f040003;
         public static final int anneString=0x7f040002;
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
